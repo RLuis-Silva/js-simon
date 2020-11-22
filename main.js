d6f9a3edefbab8208149290d9b4538e9947632ea
Luis <s.ros.lui@gmail.com>
2020-11-22 16:16:07 +0100

    add comment

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
 
-    //Todo 1- Un alert espone 5 numeri casuali diversi.
-    //Todo 2 - Dopo 30 secondi l'utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-    //Todo 3 - Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+/**
+ * un alert espone 5 numeri casuali diversi.
+ * dopo 30 secondi l' utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+ * una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+ *  */  
 
-
-    // Var 
     var randomNumber = $('#numRandom');
     var collectionNumbers = [];
     var minNumber = 1;
@@ -15,8 +15,7 @@ $(document).ready(function(){
     var playerNumber;
     var score = [];
 
-    // Creazione numeri random da parte del computer
-
+    // Creazione numeri random 
     for ( i = 0; i < 5; i++){
        var selectNumber = Math.floor(Math.random () *( maxNumber - minNumber + 1)) + minNumber;
        //check number già non presente
@@ -25,11 +24,9 @@ $(document).ready(function(){
        }
         collectionNumbers.push(selectNumber);
     };
-    console.log(collectionNumbers); //?debug
+    console.log(collectionNumbers); 
 
     // Mostra i numeri su schermo per 30 secondi
-
-    
     var timer = setInterval(function(){
         if (secondi == 0){
             randomNumber.hide();
@@ -37,16 +34,14 @@ $(document).ready(function(){
 
         } else {
             randomNumber.text(collectionNumbers);
-            console.log(secondi); //? check secondi debug
+            console.log(secondi); 
             secondi --;
         }
 
     },1000);
 
-    // Chiedi al giocatore di inserire per 5 volte un numero
-
+    // Chiede al giocatore di inserire per 5 volte un numero
     setTimeout (gamestart, secondi*1200);
-
     function gamestart () {
         for (i = 0; i < 5; i++){
             playerNumber = parseInt( prompt('Adesso tocca a te! \nInserisci un numero'));
@@ -58,10 +53,10 @@ $(document).ready(function(){
             }
         } 
         // Il software dice quali sono i numeri che sono stati indovinati
-        console.log('i numeri corretti da te inseriti sono ', score);
-        console.log('Hai indovinato nua quantità di numeri pari a ', score.length);
-        $('#risultatoNumLista').text('Ecco i numeri che hai indovinato! ' + score);
-        $('#risultatoPunteggio').text('Il totale dei numeri corretti che hai indovinato è ' + score.length);
+        console.log('numeri corretti inseriti sono: ', score);
+        console.log('Hai indovinato quantità num: ', score.length);
+        $('#risultatoNumLista').text('Ecco hai indovinato i seguenti numeri! ' + score);
+        $('#risultatoPunteggio').text('In totale hai indovinato quantità num: ' + score.length);
     }
 
 }); // <--- end doc ready
